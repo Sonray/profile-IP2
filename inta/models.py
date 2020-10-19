7353e6462bc51fb47d16a4884d2d36916001c6c5
David Okwacha <osweredavid@gmail.com>
2020-10-19 14:24:06 +0300

    created profile model

--- inta/models.py
+++ inta/models.py
@@ -34,3 +34,40 @@ class Image(models.Model):
         img = Image.objects.filter(profile=user_id).all()
         sort = sorted(img, key=lambda t: t.created_on)
         return sort
+
+class Profile(models.Model):
+    ''' extended User model '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(default='default.jpg', upload_to='avatars/')
+    bio = models.TextField(max_length=500, blank=True, default=f'Hello, I am new here!')
+
+    def __str__(self):
+        return f'{self.user.username}'
+
+    def save_profile(self):
+        ''' method to save a user's profile '''
+        self.save()
+
+    def delete_profile(self):
+        '''method to delete a user's profile '''
+        self.delete()
+
+    def update_bio(self, new_bio):
+        ''' method to update a users profile bio '''
+        self.bio = new_bio
+        self.save()
+
+    def update_image(self, user_id, new_image):
+        ''' method to update a users profile image '''
+        user = User.objects.get(id=user_id)
+        self.photo = new_image
+        self.save()
+        
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
