70b166512193e627a42c48787717b1ebcc284359
David Okwacha <osweredavid@gmail.com>
2020-10-19 14:55:27 +0300

    modified the model

--- inta/models.py
+++ inta/models.py
@@ -1,111 +1,59 @@
-from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 # Create your models here.
-class Image(models.Model):
-    ''' a model for Image posts '''
-    image = models.ImageField(upload_to='images/')
-    caption = models.TextField()
-    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, blank=True)
-    created_on = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['?'] #order randomly on feed
-
-    def save_image(self):
-        ''' method to save an image post instance '''
-        self.save()
-
-    def delete_image(self):
-        '''method to delete an image post instance '''
-        self.delete()
-
-    def update_caption(self, new_caption):
-        ''' method to update an image's caption '''
-        self.caption = new_caption
-        self.save()
-        
-    @classmethod    
-    def get_user_images(cls, user_id):
-        ''' method to retrieve all images'''
-        img = Image.objects.filter(profile=user_id).all()
-        sort = sorted(img, key=lambda t: t.created_on)
-        return sort
-
 class Profile(models.Model):
-    ''' extended User model '''
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    photo = models.ImageField(default='default.jpg', upload_to='avatars/')
-    bio = models.TextField(max_length=500, blank=True, default=f'Hello, I am new here!')
-
-    def __str__(self):
-        return f'{self.user.username}'
+    profile_picture = models.ImageField(upload_to = 'profile_pictures/', blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    bio = models.TextField(max_length = 150, blank=True)
+    followers = models.ManyToManyField(User,related_name='followers', blank=True)
 
     def save_profile(self):
-        ''' method to save a user's profile '''
+        self.save()
+
+    def update_bio(self):
+        self.bio = bio
         self.save()
 
     def delete_profile(self):
-        '''method to delete a user's profile '''
         self.delete()
 
-    def update_bio(self, new_bio):
-        ''' method to update a users profile bio '''
-        self.bio = new_bio
+    def __str__(self):
+        return str(self.user)
+
+    @classmethod
+    def search_user(cls, username):
+        user = User.objects.get(username = username)
+        return user
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    image_name = models.CharField(max_length = 30)
+    image_caption = models.TextField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, blank=True,related_name='post_likes')
+    upload_date = models.DateTimeField(auto_now_add=True,null=True) 
+
+    def __str__(self):
+        return self.image_name
+
+    @classmethod
+    def get_images(cls):
+        images = cls.objects.all().order_by('-upload_date')
+        return images
+
+    def save_image(self):
         self.save()
 
-    def update_image(self, user_id, new_image):
-        ''' method to update a users profile image '''
-        user = User.objects.get(id=user_id)
-        self.photo = new_image
+    def delete_image(self):
+        self.delete()
+
+    def update_caption(self, caption):
+        self.image_caption = caption
         self.save()
-        
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
 
 class Comment(models.Model):
-    ''' a model for comments'''
-    related_post = models.ForeignKey('Image', on_delete=models.CASCADE)
-    name = models.ForeignKey('Profile', on_delete=models.CASCADE)
-    comment_body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['created_on']
-
-    def save_comments(self):
-        ''' method to save comment instance '''
-        self.save()
-
-    def delete_comment(self):
-        '''method to delete comment instance '''
-        self.delete()
-    
-    def edit_comment(self, new_comment):
-        ''' method to edit a comment '''
-        self.comment_body = new_comment
-        self.save()
-
-    def __str__(self):
-        return f'Comment by {self.name}'
-
-class Relation(models.Model):
-    ''' model for user relations: follower-following system'''
-    follower = models.ForeignKey('Profile', related_name='following', on_delete=models.CASCADE)
-    followed = models.ForeignKey('Profile', related_name='followers', on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('follower', 'followed')
-
-    def __str__(self):
-        return '{self.follower} follows {self.followed}'
+    comment = models.CharField(max_length=2200)
+    image = models.IntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
